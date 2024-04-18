8d7f80067eb2e9376a6181ece9af20ba47579f24
Filip <filee@hotmail.se>
2024-04-18 11:46:13 +0200

    main added

--- direction_finding_connectionless_tx_2/src/main.c
+++ direction_finding_connectionless_tx_2/src/main.c
@@ -18,7 +18,7 @@
 /* Length of CTE in unit of 8[us] */
 #define CTE_LEN (0x14U)
 /* Number of CTE send in single periodic advertising train */
-#define PER_ADV_EVENT_CTE_COUNT 5
+#define PER_ADV_EVENT_CTE_COUNT 1
 
 static void adv_sent_cb(struct bt_le_ext_adv *adv,
 			struct bt_le_ext_adv_sent_info *info);
@@ -28,6 +28,26 @@ const static struct bt_le_ext_adv_cb adv_callbacks = {
 };
 
 static struct bt_le_ext_adv *adv_set;
+
+static uint16_t minAdvInterval;
+static uint16_t maxAdvInterval;
+
+static uint16_t advIntervals[] = {50, 100, 250, 1000};
+static uint8_t advIntervalIndex = 0;
+
+static struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+    BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0xaa, 0xfe),
+    BT_DATA_BYTES(BT_DATA_SVC_DATA16,
+                  0xaa, 0xfe, /* Eddystone UUID */
+                  0x00, /* Eddystone-UID frame type */
+                  0x00, /* TX Power */
+                  'N', 'I', 'N', 'A', '-', 'B', '4', 'T', 'A', 'G', /* Namespace placeholder, will be replaced in init */
+                  'i', 'n', 's', 't', 'a', 'e', /* Instance Id placeholder, will be replaced in init */
+                  0x00, /* reserved */
+                  0x00 /* reserved */
+                 )
+};
 
 const static struct bt_le_adv_param param =
 		BT_LE_ADV_PARAM_INIT(BT_LE_ADV_OPT_EXT_ADV |
@@ -41,31 +61,16 @@ static struct bt_le_ext_adv_start_param ext_adv_start_param = {
 	.num_events = 0,
 };
 
-const static struct bt_le_per_adv_param per_adv_param = {
-	.interval_min = BT_GAP_ADV_SLOW_INT_MIN,
-	.interval_max = BT_GAP_ADV_SLOW_INT_MAX,
-	.options = BT_LE_ADV_OPT_USE_TX_POWER,
-};
 
-#if defined(CONFIG_BT_DF_CTE_TX_AOD)
-/* Example sequence of antenna switch patterns for antenna matrix designed by
- * Nordic. For more information about antenna switch patterns see README.rst.
- */
-static uint8_t ant_patterns[] = {0x2, 0x0, 0x5, 0x6, 0x1, 0x4, 0xC, 0x9, 0xE,
-				 0xD, 0x8, 0xA};
-#endif /* CONFIG_BT_DF_CTE_TX_AOD */
+
+
+
 
 struct bt_df_adv_cte_tx_param cte_params = { .cte_len = CTE_LEN,
-					     .cte_count = PER_ADV_EVENT_CTE_COUNT,
-#if defined(CONFIG_BT_DF_CTE_TX_AOD)
-					     .cte_type = BT_DF_CTE_TYPE_AOD_2US,
-					     .num_ant_ids = ARRAY_SIZE(ant_patterns),
-					     .ant_ids = ant_patterns
-#else
-					     .cte_type = BT_DF_CTE_TYPE_AOA,
-					     .num_ant_ids = 0,
-					     .ant_ids = NULL
-#endif /* CONFIG_BT_DF_CTE_TX_AOD */
+           .cte_count = PER_ADV_EVENT_CTE_COUNT,
+           .cte_type = BT_DF_CTE_TYPE_AOA,
+           .num_ant_ids = 0,
+           .ant_ids = NULL
 };
 
 static void adv_sent_cb(struct bt_le_ext_adv *adv,
@@ -77,6 +82,9 @@ static void adv_sent_cb(struct bt_le_ext_adv *adv,
 
 int main(void)
 {
+	minAdvInterval = 50 / 1.25;
+    maxAdvInterval = 50 / 1.25;
+
 	char addr_s[BT_ADDR_LE_STR_LEN];
 	struct bt_le_oob oob_local;
 	int err;
@@ -100,6 +108,12 @@ int main(void)
 	}
 	printk("success\n");
 
+	printk("Set ext adv data...");
+    err = bt_le_ext_adv_set_data(adv_set, ad, ARRAY_SIZE(ad), NULL, 0);
+    if (err) {
+        printk("Failed setting ext adv data: %d\n", err);
+    }
+
 	printk("Update CTE params...");
 	err = bt_df_set_adv_cte_tx_param(adv_set, &cte_params);
 	if (err) {
@@ -107,6 +121,14 @@ int main(void)
 		return 0;
 	}
 	printk("success\n");
+	minAdvInterval = advIntervals[advIntervalIndex]/ 1.25;
+	maxAdvInterval = advIntervals[advIntervalIndex] / 1.25;
+
+	struct bt_le_per_adv_param per_adv_param = {
+        .interval_min = minAdvInterval,
+        .interval_max = maxAdvInterval,
+        .options = BT_LE_ADV_OPT_USE_TX_POWER,
+    };
 
 	printk("Periodic advertising params set...");
 	err = bt_le_per_adv_set_param(adv_set, &per_adv_param);
