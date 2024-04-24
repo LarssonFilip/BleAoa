b37cb632ee2bb0a2e9370bb3a03e8de32b61d944
Filip <filee@hotmail.se>
2024-04-24 17:00:16 +0200

    new code

--- antenna/antenna.py
+++ antenna/antenna.py
@@ -4,27 +4,30 @@ import time
 import numpy as np
 
 
-
+BUFFSIZE = 20
 
 
 class Antenna:
 
+    
    
 
     def __init__(self, baudrate, port, pos, id):
 
+        self.buff = []
+        self.buffix = 0
         self.pos = pos
         self.id = id
         self.baudrate = baudrate
         self.port = port
-        self.x = np.linspace(-100, 400, 100)
         self.y = 0
+        self.x = np.linspace(-100, 400, 1000)
         self.deg = 0
         self.cDeg = 0
+        self.rssi = -56
         self.parameters = []
         self.convAngle(self.deg)
         self.switch()
-        print(self.y)
         
 
 
@@ -46,8 +49,8 @@ class Antenna:
             if string != "" or string != "\n":
                 if "UUDF" in string:
                     self.parameters = string.split(",")
-                    print("values = " + str(self.parameters[2]))
                     self.convAngle(self.parameters[2])
+                    self.rssi = self.parameters[1]
                     self.switch()
                 else: 
                     print(string.strip())
@@ -58,9 +61,18 @@ class Antenna:
         #Remove to use antenna
 
     def convAngle(self,angle):
-        self.deg = 90 - int(angle)
-        print(self.deg)
-    
+        if self.buffix != BUFFSIZE:
+            self.buff[self.buffix] = int(angle)
+            self.buffix + 1
+        else:
+            self.buffix = 0
+            self.buff[self.buffix] = int(angle)
+        average = np.average(self.buff)
+        print(average)
+        self.deg = 90 - average
+
+
+        
     def switch(self):
         if self.pos == 0:
             self.cDeg = self.deg + 45
--- antenna/main.py
+++ antenna/main.py
@@ -9,40 +9,105 @@ import matplotlib.patches as patches
 
 global antennas
 
-antennas:Antenna = []
-#antennas.append(Antenna(115200,"COM5",0,0)) 
 
+measuredPower = -68
+N = 2
+
+antennas:Antenna = []
+#antennas.append(Antenna(115200,"COM5",3,0)) 
+#antennas.append(Antenna(115200,"COM5",1,1)) 
+#antennas.append(Antenna(115200,"COM5",2,2)) 
 
 # Test daat for plotting antenna calculations
 
 def drawGraph():
     
+    funcs = []
+    xV = []
+    yV = []
  
     # Creating vectors X and Y
+    x = np.linspace(-100, 400, 1000)
     
-    
-
     fig, ax = plt.subplots()
-    square = patches.Rectangle((0, 0), 200, 300, edgecolor='orange', facecolor='none')
+    square = patches.Rectangle((0, 0), 200, 300, edgecolor='orange', facecolor='none',)
     ax.add_patch(square)
-    
-    
+
     # Create the plot
     plt.xlim(-100, 300)
-    plt.ylim(-100, 400)   
-    
-    x1 = antennas[0].x
-    y1 = antennas[0].y
-    x2 = antennas[1].x
-    y2 = antennas[1].y
-    plt.plot(x1,y1)
-    plt.plot(x2,y2)
+    plt.ylim(-100, 400)
 
-    
+    if(len(antennas) <= 1):
+        
+        plt.plot(antennas[0].x, antennas[0].y)
+        distance = np.power(10, ((measuredPower-antennas[0].rssi)/(10*N)))
+        print(distance)
+        #draw a circle
+        pos = antennas[0].pos
+        if pos == 0:
+            xOff = 0
+            yOff = 300
+        elif pos == 1:
+            xOff = 100
+            yOff = 300
+        elif pos == 2:
+            xOff = 200
+            yOff = 300
+        elif pos == 3:
+            xOff = 0
+            yOff = 150
+        elif pos == 4:
+            xOff = 200
+            yOff = 150
+        elif pos == 5:
+            xOff = 0
+            yOff = 0
+        elif pos == 6:
+            xOff = 100
+            yOff = 0
+        elif pos == 7:
+            xOff = 200
+            yOff = 0
+
+        angles = np.linspace(0 * np.pi, 2 * np.pi, 100 )
+        r = 100 * distance
+        xs = r * np.cos(angles) + xOff
+        ys = r * np.sin(angles) + yOff
+        plt.plot(xs, ys, color = 'green')
+
+
+
+    else:
+
+        for antenna in antennas:
+            y = antenna.y
+            funcs.append(y)
+            plt.plot(x,y)
+
+            
+        for i, func in enumerate(funcs):
+            for j, fun in enumerate(funcs):
+                if(i != len(funcs)-1):
+                    if(j > i):
+                        idx = np.argwhere(np.diff(np.sign(funcs[i] - funcs[j]))).flatten()
+                        
+                        plt.plot(x[idx], funcs[i][idx], 'ro')  
+                        xV.append(x[idx])
+                        yV.append(funcs[i][idx])
+        
+        
+        xAverage = np.average(xV)
+        print("y values")
+        yAverage = np.average(yV)
+        print(xAverage)
+        print(yAverage)
+        plt.plot(xAverage, yAverage, 'ro', color='b')
+
     # function to show the plot
+
     plt.show()
 
-    #End of test Data
+        #End of test Data
     
 
 def addAntenna(string):
